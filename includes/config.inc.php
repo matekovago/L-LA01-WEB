fa4e9b29a66b64472cbaba4c2c0825d85f731f92
mate-kovago <matekovago@gmail.com>
2025-03-09 20:33:55 +0100

    create file structure and start Front Controller

---
+++ includes/config.inc.php
@@ -0,0 +1,29 @@
+<?php
+$ablakcim = array(
+    'cim' => 'Mini honlap Kft.',
+);
+
+$fejlec = array(
+    'kepforras' => 'logo.png',
+    'kepalt' => 'logo',
+	'cim' => 'Mini honlap',
+	'motto' => ''
+);
+
+$lablec = array(
+    'copyright' => 'Copyright '.date("Y").'.',
+    'ceg' => 'Mini honlap Kft.'
+);
+
+$oldalak = array(
+	'/' => array('fajl' => 'cimlap', 'szoveg' => 'Címlap', 'menun' => array(1,1)),
+    'regisztral' => array('fajl' => 'regisztral', 'szoveg' => 'Regisztráció', 'menun' => array(1,1)),
+    'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés', 'menun' => array(1,1)),
+    'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(1,1)),
+    'kepek' => array('fajl' => 'kepek', 'szoveg' => 'Képek', 'menun' => array(1,1)),
+	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
+    'uzenet' => array('fajl' => 'uzenet', 'szoveg' => 'Üzenetek', 'menun' => array(1,1)),
+);
+
+$hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
+?>
---
+++ templates/pages/regisztral.tpl.php
@@ -0,0 +1,1 @@
+<h2>Regisztrál</h2>
