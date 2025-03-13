cceeabcc37cbdb68cf81c677fd8ab91f955bbb82
matekovago <dzmwso@hallgato.uni-neumann.hu>
2025-03-13 21:29:34 +0100

    add login point and login PHP

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -23,6 +23,7 @@ $oldalak = array(
     'kepek' => array('fajl' => 'kepek', 'szoveg' => 'Képek', 'menun' => array(1,1)),
 	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
     'uzenet' => array('fajl' => 'uzenet', 'szoveg' => 'Üzenetek', 'menun' => array(1,1)),
+    'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0,0)),
 );
 
 $hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
--- templates/pages/belepes.tpl.php
+++ templates/pages/belepes.tpl.php
@@ -1,1 +1,11 @@
 <h2>Belepes</h2>
+<form action = "?belep" method = "post">
+<fieldset>
+<legend>Bejlentkezés</legend>
+<br>
+<input type="text" name="felhasznalo" placeholder="felhasználó" required><br><br>
+<input type="password" name="jelszo" placeholder="jelszó" required><br><br>
+<input type="submit" name="belepes" value="Belépés">
+<br>&nbsp;
+</fieldset>
+</form>
